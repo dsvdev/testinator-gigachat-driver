a821fea19846daffa63bb9ff44269287ce9105d0
60237302 <dmitriy.sviridov_ext@lemanapro.ru>
2025-06-19 23:55:14 +0300

    init commit

---
+++ pkg/llm/gigachat.go
@@ -0,0 +1,13 @@
+package llm
+
+type GigachatDriver struct {
+	token string
+}
+
+func NewGigachatDriver(token string) *GigachatDriver {
+	return &GigachatDriver{token: token}
+}
+
+func (d *GigachatDriver) SendRequest(prompt string) (string, error) {
+	return "", nil
+}
